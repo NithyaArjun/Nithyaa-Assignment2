5ac28857a944d5fb924afd2276bdaa945b531f26
NithyaArjun <nithyakpa@gmail.com>
2024-03-25 08:21:35 -0400

    Initial commit

---
+++ Assignment2/src/com/coderscampus/GameTest.java
@@ -0,0 +1,42 @@
+package com.coderscampus;
+
+import java.util.Scanner;
+
+public class GameTest {
+	
+	
+	
+	public void game(int randomnumber,int guessnumber ,int i) {
+			
+		if(i<4) {
+		
+		 if(randomnumber == guessnumber)
+		{
+			
+			System.out.println("You win");
+			
+			return;
+			
+		}
+		
+		else if(guessnumber > randomnumber) {
+	
+			
+			System.out.println("Please pick a lower number"); 
+			
+			}
+		else if(guessnumber < randomnumber) {
+			
+			System.out.println("Please pick a higher number");
+		}
+		}
+		
+		else {
+			System.out.println("You lose the number to guess was  " +randomnumber +"the  actual number");
+		
+		}
+		
+		}
+		
+
+}
--- Assignment2/src/module-info.java
+++ Assignment2/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module Random {
+module Assignment2 {
 }
